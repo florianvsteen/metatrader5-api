846f8d2f4d91927d9e56ee8f82dacef033c07fbb
florianvsteen <florian@flownet.be>
2026-03-15 11:31:24 +0100

    Update 02-common.sh

--- backend/mt5/scripts/02-common.sh
+++ backend/mt5/scripts/02-common.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Set variables
 mt5setup_url="https://download.mql5.com/cdn/web/metaquotes.software.corp/mt5/mt5setup.exe"
 mt5file="/config/.wine/drive_c/Program Files/MetaTrader 5/terminal64.exe"
@@ -12,7 +11,7 @@ mt5server_port=18812
 log_message() {
     local level=$1
     local message=$2
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - [$level] $message" >> /var/log/mt5_setup.log
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - [$level] $message" | tee -a /tmp/mt5_setup.log
 }
 
 # Function to check if a Python package is installed in Wine
